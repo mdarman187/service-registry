a82e29ae7bd05d2325ee6948ec1b26dabbfd601c
Md Arman <muhammadarman187@gmail.com>
2021-10-28 23:09:40 +0530

    first commit

---
+++ src/test/java/com/arman/microservices/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arman.microservices.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
